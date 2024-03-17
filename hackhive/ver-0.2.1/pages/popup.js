c83570d24109ea7f15fcaa04cf10dd1c31f59ad4
Mohit <mohitsinghsahare@gmail.com>
2024-03-17 12:47:01 +0530

    ver 0.3.2, export transcriprt works

---
+++ hackhive/ver-0.2.1/pages/popup.js
@@ -0,0 +1,17 @@
+console.log("working")
+document.addEventListener('DOMContentLoaded', () => {
+    chrome.storage.local.get(['isMeetPage'], function(result) {
+        if (result.isMeetPage) {
+            document.getElementById('message').textContent = 'Ready for a Google Meet!';
+        } else {
+            document.getElementById('message').textContent = 'This page is not a Google Meet page.';
+        }
+    });
+});
+
+document.getElementById('export-transcript').addEventListener('click', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {action: "exportTranscript"});
+    });
+});
+
